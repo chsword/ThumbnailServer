78f5bac27d0acfd491d2a261d45dd99177b4faa3
chsword <chsword@126.com>
2014-05-09 14:44:36 +0800

    flunt config

---
+++ ThumbnailServer/Chsword.ThumbnailServer/ThumbnailConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Chsword.ThumbnailServer
+{
+    public class ThumbnailConfig
+    {
+        static ThumbnailConfig Instance { get; set; }
+        public static ThumbnailConfig Start()
+        {
+            if (Instance == null)
+            {
+                Instance = new ThumbnailConfig();
+            }
+            return Instance;
+        }
+        public ThumbnailConfig Include(string path, Func<ThumbnailSetting, ThumbnailSetting> setting)
+        {
+            return this;
+        }
+    }
+
+    public class ThumbnailSetting
+    {
+    }
+}
